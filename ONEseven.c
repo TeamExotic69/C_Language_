c83e4f6937bfedc9a708befcfa967d77ae64415e
TeamExotic69 <149905379+TeamExotic69@users.noreply.github.com>
2023-11-20 22:15:56 +0530

    Update ONEseven.c

--- ONEseven.c
+++ ONEseven.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
+// #include<conio.h>
 void main(){
-    int x,i;
+    int x,i,j;
+    // clrscr();
     printf("Enter number of times you want to print patern: ");
     scanf("%d",&x);
     for(i=0;i<x;i++){
         printf("\n");
-        for(int j=i;j>0;j--){
+        for(j=i;j>0;j--){
             printf("*");
         }
     }
