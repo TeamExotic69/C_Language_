fae656a442b8cb36fba210ba6fd00de50e12b117
TeamExotic69 <149905379+TeamExotic69@users.noreply.github.com>
2023-11-20 23:00:54 +0530

    Update ONEeight.c

--- ONEeight.c
+++ ONEeight.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
+// #include<conio.h>
 void main(){
     int x,i,j;
+    // clrscr();
     printf("Enter number of times you want to print patern: ");
     scanf("%d",&x);
     for(i=1;i<=x;i++){
